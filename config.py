2026e485b6bf2b9dfe45d2aaeeec0a2a5be0439e
Arslan Ovezov <arslan.datascience@gmail.com>
2026-02-21 16:54:21 +0500

    changed config

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
-    gemini_api_key = ""
+    gemini_api_key = os.getenv("gemini_api_key")
     
     
 config_obj = Config()
